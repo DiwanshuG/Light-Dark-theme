05de227866e6817821ec0d2bf51fb1d026b8c61d
Diwanshu Gangwar <118631453+DiwanshuG@users.noreply.github.com>
2024-09-22 00:08:56 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LightDarkMode from "./components/theme-switch";
+function App() {
+  return (
+    <div className="App">
+      {/* light-dark-theme-container */}
+      <LightDarkMode />
+    </div>
+  );
+}
+
+export default App;
